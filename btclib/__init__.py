440d0d6db7a4885f44987f5029acceb6081c406b
qwerty <itsqwz@gmail.com>
2021-11-07 21:09:02 +0300

    Add NetworkAPI in btclib __init__

--- btclib/__init__.py
+++ btclib/__init__.py
@@ -2,3 +2,4 @@ from btclib.address import PrivateKey, PublicKey, P2PKH, P2SH, P2WPKH, P2WSH, Ad
 from btclib.transaction import Input, Output, Transaction, get_inputs
 from btclib.script import Script
 from btclib import bech32
+from btclib.services import NetworkAPI
