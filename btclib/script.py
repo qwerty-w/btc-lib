bf5ded4a102f5808cfa3bee8857dd8745cce1843
qwerty <itsqwz@gmail.com>
2023-12-09 15:25:48 +0300

    Update and refactor script.py, make Script more like list

--- btclib/script.py
+++ btclib/script.py
@@ -1,108 +1,146 @@
-from typing import Iterable, Optional
+from collections.abc import Iterable
+from typing import Iterable, Iterator, Optional, SupportsIndex, overload
 
 from btclib.const import OP_CODES, CODE_OPS
 from btclib.utils import dint, pprint_class
 from btclib import exceptions
 
 
-class Script:
-    def __init__(self, *data: str | bytes | int):
-        self.script = tuple(self._validate_data(data))
+type InputItem = str | bytes | list[int]
 
-    def __repr__(self) -> str:
-        return pprint_class(self, self.script)
 
-    def __len__(self) -> int:
-        return len(self.script)
+def validate(item: InputItem) -> Optional[str]:
+    match item:
+        case _ if not isinstance(item, (str, bytes, list)):
+            raise exceptions.InvalidInputScriptData(type(item))
 
-    def __eq__(self, other: 'Script'):
-        if isinstance(other, Script):
-            return self.script == other.script
+        case _ if not len(item):  # empty
+            return None
 
-        return NotImplemented
+        case list():  # list[int]
+            return bytes(item).hex()
 
-    @staticmethod
-    def _validate_data(raw: Iterable) -> list[str]:
-        script = []
+        case str() if item.startswith('OP_'):  # opcode
+            if item not in OP_CODES:
+                raise ValueError(f'unknown opcode: {item}')
 
-        for item in raw:
-            if not len(item):
-                continue
+            return item
 
-            if isinstance(item, str):
-                if not item.startswith('OP_'):
-                    if len(item) % 2 != 0:
-                        raise exceptions.HexLengthMustBeMultipleTwo
+        case str():  # hex
+            if len(item) % 2 > 0:
+                raise exceptions.HexLengthMustBeMultipleTwo
 
-                    try:
-                        bytes.fromhex(item)
-                    except ValueError:
-                        raise exceptions.InvalidHexOrOpcode(item) from None
+            try:
+                bytes.fromhex(item)
+            except ValueError:
+                raise exceptions.InvalidHexOrOpcode(item) from None
+            
+            return item
 
-            elif isinstance(item, bytes):
-                item = item.hex()
+        case bytes():  # bytes
+            return item.hex()
 
-            else:
-                raise exceptions.InvalidInputScriptData(type(item)) from None
 
-            script.append(item)
+def validator(script: Iterable[InputItem]) -> Iterator[str]:
+    for item in script:
+        valid = validate(item)
+        if not valid:
+            continue
+        yield valid
 
-        return script
+
+class Script(list[str]):
+    def __init__(self, *data: InputItem) -> None:
+        super().__init__(validator(data))
 
     @classmethod
-    def from_raw(cls, data: str | bytes | list[int], *, segwit: bool = False,
-                 max_items_count: Optional[int] = None) -> 'Script':
+    def deserialize(cls, raw: InputItem, *, segwit: bool = False,
+                 max_items: Optional[int] = None) -> 'Script':
         script = []
-        data = bytes.fromhex(data) if isinstance(data, str) else bytes(data) if isinstance(data, list) else data
+        data = bytes.fromhex(raw) if isinstance(raw, str) else bytes(raw) if isinstance(raw, list) else raw
 
         count = 0
-        while len(data) > 0 and ((count < max_items_count) if max_items_count is not None else True):
-            first_byte = data[0:1]
+        while len(data) > 0 and (max_items is None or count < max_items):
+            fb = data[0:1]
+            op = CODE_OPS.get(fb)
 
             # if <opcode> <data>
-            op = CODE_OPS.get(first_byte)
             if op and not segwit:
-                item, item_size = op, 1
+                item, size = op, 1
 
+            # if <size> <opcode/bytes>
             else:
-                item_size, data = dint.unpack(data, increased_separator=segwit)
-                item = data[:item_size]
+                size, data = dint.unpack(data, increased_separator=segwit)
+                item = data[:size]
 
                 # if <opcode size> <opcode> <data>
-                op = 'OP_0' if item_size == 0 else CODE_OPS.get(item) if item_size == 1 else None
+                op = 'OP_0' if size == 0 else CODE_OPS.get(item) if size == 1 else None
                 item = op if op is not None else item
 
             script.append(item)
-            data = data[item_size:]
-
+            data = data[size:]
             count += 1
 
         return cls(*script)
 
     def is_empty(self) -> bool:
-        return len(self) == 0
+        return not len(self)
 
-    def _stream(self, *, segwit: bool = False) -> bytes:
-        serialized_data = b''
+    def serialize(self, *, segwit: bool = False) -> bytes:
+        b = b''
 
-        for item in self.script:
-            if item in ('00', 'OP_0'):
-                serialized_data += b'\x00'
-                continue
+        for item in self:
+            match item:
+                case '00' | 'OP_0':
+                    b += b'\x00'
 
-            op = OP_CODES.get(item) if item.startswith('OP_') else None
-            if op:
-                # <op size> + <op> if segwit else <op>
-                serialized_data += (b'\x01' if segwit else b'') + op
-                continue
+                case _ if item.startswith('OP_'):
+                    # <op size> + <op> if segwit else <op>
+                    b += (b'\x01' if segwit else b'') + OP_CODES[item]
 
-            item = bytes.fromhex(item)
-            serialized_data += dint(len(item)).pack(increased_separator=segwit) + item
+                case _:
+                    item = bytes.fromhex(item)
+                    b += dint(len(item)).pack(increased_separator=segwit) + item
 
-        return serialized_data
+        return b
 
-    def to_bytes(self, *, segwit: bool = False) -> bytes:
-        return self._stream(segwit=segwit)
+    def append(self, __object: InputItem) -> None:
+        _object = validate(__object)
+        return super().append(_object) if _object else None
 
-    def to_hex(self, *, segwit: bool = False) -> str:
-        return self.to_bytes(segwit=segwit).hex()
+    def extend(self, __iterable: Iterable[InputItem]) -> None:
+        return super().extend(validator(__iterable))
+    
+    def insert(self, __index: SupportsIndex, __object: InputItem) -> None:
+        _object = validate(__object)
+        return super().insert(__index, _object) if _object else None
+    
+    def copy(self) -> 'Script':
+        return Script(*super().copy())
+
+    def __repr__(self) -> str:
+        return pprint_class(self, args=self)
+
+    def __eq__(self, other: 'Script') -> bool:
+        if not isinstance(other, Script):
+            return False
+        return super().__eq__(other)
+
+    def __add__(self, __object: 'Script') -> 'Script':
+        if not isinstance(__object, Script):
+            raise TypeError(f'can only concatenate Script (not "{type(__object).__name__}") to Script')
+        return Script(*self, *__object)
+
+    def __iadd__(self, __value: Iterable[InputItem]) -> 'Script':
+        return super().__iadd__(validator(__value))
+
+    @overload
+    def __setitem__(self, __key: SupportsIndex, __value: InputItem) -> None: ...
+    @overload
+    def __setitem__(self, __key: slice, __value: Iterable[InputItem]) -> None: ...
+    def __setitem__(self, __key, __value) -> None:
+        if isinstance(__key, SupportsIndex):
+            return super().__setitem__(__key, __v) if (__v := validate(__value)) else None
+        if isinstance(__key, slice):
+            return super().__setitem__(__key, validator(__value))
+        raise TypeError(f'Script indices must be integers or slices, not {type(__key).__name__}')
