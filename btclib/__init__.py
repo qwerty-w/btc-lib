6095aa6f0223aa1ad113f8bdb87730cbd36afdf0
qwerty <itsqwz@gmail.com>
2023-12-20 15:51:59 +0300

    Move get_unspent_inputs in __init__

--- btclib/__init__.py
+++ btclib/__init__.py
@@ -1,8 +1,13 @@
-from btclib.address import PrivateKey, PublicKey, P2PKH, P2SH, P2WPKH, P2WSH, Address
-from btclib.transaction import Input, Output, Transaction, get_inputs
+from btclib import bech32, utils
 from btclib.script import Script
-from btclib import bech32
 from btclib.services import NetworkAPI
+from btclib.address import PrivateKey, PublicKey, P2PKH, P2SH, P2WPKH, P2WSH, Address
+from btclib.transaction import RawInput, UnsignableInput, Input, Output, RawTransaction, Transaction
 
 
 __version__ = '0.0.2'
+
+
+def get_unspent_inputs(*args: tuple[PrivateKey, Address]) -> list['Input']:
+    return [Input.from_unspent(unspent, pv, address) for pv, address in args for unspent in address.get_unspents()]
+
